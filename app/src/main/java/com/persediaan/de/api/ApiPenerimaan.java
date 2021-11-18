737ea4bf99f57a25bd167bc699619969411b53b4
Deo Artanta <armadadeo064@gmail.com>
2021-11-19 03:22:46 +0700

    Perbaikan bug penerimaan

---
+++ app/src/main/java/com/persediaan/de/api/ApiPenerimaan.java
@@ -0,0 +1,119 @@
+package com.persediaan.de.api;
+
+public class ApiPenerimaan {
+    private String id_purchase,note,dt_purchase,
+            nm_area,nm_singkat,nm_item,nm_satuan,
+            eceran,nm_suplier,alasuplier,npwp,
+            name_penyedia,area,alamat,id_trans;
+
+    private int admin,id,id_area,id_suplier,id_item,qty,id_satuan,
+            harga,diterima,jumlah,harga_total=0,tgl_purchase;
+
+    public String getId_purchase() {
+        return id_purchase;
+    }
+
+    public int getTgl_purchase() {
+        return tgl_purchase;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public String getDt_purchase() {
+        return dt_purchase;
+    }
+
+    public String getNm_area() {
+        return nm_area;
+    }
+
+    public String getNm_singkat() {
+        return nm_singkat;
+    }
+
+    public String getNm_item() {
+        return nm_item;
+    }
+
+    public String getNm_satuan() {
+        return nm_satuan;
+    }
+
+    public String getEceran() {
+        return eceran;
+    }
+
+    public String getNm_suplier() {
+        return nm_suplier;
+    }
+
+    public String getAlasuplier() {
+        return alasuplier;
+    }
+
+    public String getNpwp() {
+        return npwp;
+    }
+
+    public String getName_penyedia() {
+        return name_penyedia;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getId_trans() {
+        return id_trans;
+    }
+
+    public int getAdmin() {
+        return admin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getId_area() {
+        return id_area;
+    }
+
+    public int getId_suplier() {
+        return id_suplier;
+    }
+
+    public int getId_item() {
+        return id_item;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public int getId_satuan() {
+        return id_satuan;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public int getDiterima() {
+        return diterima;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public int getHarga_total() {
+        return harga_total;
+    }
+}
--- app/src/main/java/com/persediaan/de/model/ModelPenerimaan.java
+++ app/src/main/java/com/persediaan/de/model/ModelPenerimaan.java
@@ -1,85 +1,85 @@
 package com.persediaan.de.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModelPenerimaan {
-    String name_penyedia,status,area,
-            alamat,tgl,id_trans,no_purchase;
-    int img_bg_card,img_bg_label,harga_total=0,
-            ppn=0,color_label,paddingBot=0,qty;
-    boolean lastItem = false;
+    String id_purchase,note,dt_purchase,
+            nm_area,nm_singkat,nm_item,nm_satuan,
+            eceran,nm_suplier,alasuplier,npwp,
+            name_penyedia,status,area,alamat,tgl,id_trans;
+
+    int admin,id,id_area,id_suplier,diterima,jumlah,harga_total=0,jml_item,tgl_purchase;
+
+    ArrayList<ModelItemBrg> modelItemBrgs;
+
     boolean expand = false;
 
     public ModelPenerimaan(
-            String name_penyedia,String status,String area,
-            String alamat,int qty,int hrg_total,String tgl,
-            String id_trans,String no_purchase,int ppn,int color_label,
-            int img_bg_card,int img_bg_label,boolean lastItem) {
-
-        this.name_penyedia = name_penyedia;this.status = status;
-        this.area = area;this.alamat = alamat;this.tgl = tgl;
-        this.img_bg_card = img_bg_card;this.img_bg_label = img_bg_label;
-        this.id_trans = id_trans;this.no_purchase = no_purchase;
-        this.qty = qty;this.harga_total = hrg_total;this.ppn = ppn;
-        this.color_label = color_label;this.lastItem = lastItem;
+            String id_purchase, int tgl_purchase, String note,
+            String dt_purchase, String nm_area, String nm_singkat,
+            String nm_suplier, String alasuplier, String npwp,
+            String name_penyedia, String status, String area, String alamat,
+            String id_trans, int jml_item, int admin, int id, int id_area,
+            int id_suplier, int diterima, int harga_total, ArrayList<ModelItemBrg> item_brg
+    ) {
+        this.id_purchase = id_purchase;this.tgl_purchase = tgl_purchase;
+        this.note = note;this.dt_purchase = dt_purchase;this.nm_area = nm_area;
+        this.nm_singkat = nm_singkat;this.nm_item = nm_item;this.nm_satuan = nm_satuan;
+        this.eceran = eceran;this.nm_suplier = nm_suplier;this.alasuplier = alasuplier;
+        this.npwp = npwp;this.name_penyedia = name_penyedia;this.status = status;
+        this.area = area;this.alamat = alamat;this.tgl = tgl;this.id_trans = id_trans;
+        this.admin = admin;this.id = id;this.id_area = id_area;this.id_suplier = id_suplier;
+        this.diterima = diterima;this.harga_total = harga_total;this.jml_item = jml_item;
+        this.modelItemBrgs = item_brg;
     }
 
-    public String getNo_purchase() {
-        return no_purchase;
+    public ArrayList<ModelItemBrg> getItemBrgs() {
+        return modelItemBrgs;
     }
 
-    public void setExpand(boolean expand) {
-        this.expand = expand;
+    public String getId_purchase() {
+        return id_purchase;
     }
 
-    public boolean isExpand() {
-        return expand;
+    public int getTgl_purchase() {
+        return tgl_purchase;
     }
 
-    public int getPaddingBot() {
-        return paddingBot;
+    public int getJml_item() {
+        return jml_item;
     }
 
-    public int getHarga_total() {
-        return harga_total;
+    public String getNote() {
+        return note;
     }
 
-    public int getPpn() {
-        return ppn;
+    public String getDt_purchase() {
+        return dt_purchase;
     }
 
-    public void setHarga_total(int harga_total) {
-        this.harga_total = harga_total;
+    public String getNm_area() {
+        return nm_area;
     }
 
-    public void setPpn(int ppn) {
-        this.ppn = ppn;
+    public String getNm_singkat() {
+        return nm_singkat;
     }
 
-    public void setImg_bg_label(int img_bg_label) {
-        this.img_bg_label = img_bg_label;
+    public String getNm_suplier() {
+        return nm_suplier;
     }
 
-    public void setColor_label(int color_label) {
-        this.color_label = color_label;
+    public String getAlasuplier() {
+        return alasuplier;
     }
 
-    public String getName_penyedia() {
-        return name_penyedia;
-    }
-
-    public String getStatus() {
-        return status;
+    public String getNpwp() {
+        return npwp;
     }
 
     public String getArea() {
         return area;
-    }
-
-    public int getQty() {
-        return qty;
-    }
-
-    public boolean isLastItem() {
-        return lastItem;
     }
 
     public String getAlamat() {
@@ -90,38 +90,87 @@ public class ModelPenerimaan {
         return tgl;
     }
 
-    public int getImg_bg_card() {
-        return img_bg_card;
-    }
-
-    public int getImg_bg_label() {
-        return img_bg_label;
-    }
-
-    public String getId_Trans() {
+    public String getId_trans() {
         return id_trans;
     }
 
-    public int getColor_label() {
-        return color_label;
+    public int getAdmin() {
+        return admin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getId_area() {
+        return id_area;
+    }
+
+    public int getId_suplier() {
+        return id_suplier;
+    }
+
+    public int getDiterima() {
+        return diterima;
+    }
+
+    public String getNm_item() {
+        return nm_item;
+    }
+
+    public String getNm_satuan() {
+        return nm_satuan;
+    }
+
+    public String getEceran() {
+        return eceran;
+    }
+
+    public void setExpand(boolean expand) {
+        this.expand = expand;
+    }
+
+    public boolean isExpand() {
+        return expand;
+    }
+
+    public int getHarga_total() {
+        return harga_total;
+    }
+
+    public String getName_penyedia() {
+        return name_penyedia;
+    }
+
+    public String getStatus() {
+        return status;
     }
 
     @Override
-    public String toString() {
+    public String  toString() {
         return "ModelPenerimaan{" +
-                "name_penyedia='" + name_penyedia + '\'' +
+                "id_purchase='" + id_purchase + '\'' +
+                ", tgl_purchase='" + tgl_purchase + '\'' +
+                ", note='" + note + '\'' +
+                ", dt_purchase='" + dt_purchase + '\'' +
+                ", nm_area='" + nm_area + '\'' +
+                ", nm_singkat='" + nm_singkat + '\'' +
+                ", nm_suplier='" + nm_suplier + '\'' +
+                ", alasuplier='" + alasuplier + '\'' +
+                ", npwp='" + npwp + '\'' +
+                ", name_penyedia='" + name_penyedia + '\'' +
                 ", status='" + status + '\'' +
                 ", area='" + area + '\'' +
-                ", qty='" + qty + '\'' +
                 ", alamat='" + alamat + '\'' +
                 ", tgl='" + tgl + '\'' +
-                ", img_bg_card=" + img_bg_card +
-                ", img_bg_label=" + img_bg_label +
                 ", id_trans='" + id_trans + '\'' +
+                ", admin=" + admin +
+                ", id=" + id +
+                ", id_area=" + id_area +
+                ", id_suplier=" + id_suplier +
+                ", diterima=" + diterima +
+                ", jumlah=" + jumlah +
                 ", harga_total=" + harga_total +
-                ", ppn=" + ppn +
-                ", color_label=" + color_label +
-                ", LastItem=" + lastItem +
                 '}';
     }
 }
